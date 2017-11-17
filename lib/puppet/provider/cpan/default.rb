e5b665af90962c39d416f92604168ee99b6b1efc
Zach Crownover <zachary.crownover@gmail.com>
2017-11-17 11:37:25 +0000

    Fix ensure with nonstandard versions
    This increases support for DragonFly BSD as well as FreeBSD, making
    them first class citizens for this module. Additionally, it creates a
    new structured fact called perl, which contains the various version
    numbers that can be referenced for what version of Perl is the system
    default. It was mainly added to properly support the config paths for
    the default @INC on DragonFly and FreeBSD. The cpan provider was
    updated to fix a few bugs around how it handled version strings, and
    using rubocop, some additional cleanup was done while there.

--- lib/puppet/provider/cpan/default.rb
+++ lib/puppet/provider/cpan/default.rb
@@ -1,38 +1,31 @@
-Puppet::Type.type(:cpan).provide( :default ) do
-  @doc = "Manages cpan modules"
+Puppet::Type.type(:cpan).provide(:default) do
+  @doc = 'Manages cpan modules'
 
-  commands :cpan     => 'cpan'
-  commands :perl     => 'perl'
-  confine  :osfamily => [:Debian, :RedHat, :Windows]
+  commands cpan: 'cpan'
+  commands perl: 'perl'
+  confine  osfamily: %i[Debian DragonFly FreeBSD RedHat Windows]
   ENV['PERL_MM_USE_DEFAULT'] = '1'
 
-  def install
-  end
+  def install; end
 
-  def force
-  end
+  def force; end
 
   def latest?
-    if resource[:local_lib]
-      ll = "-Mlocal::lib=#{resource[:local_lib]}"
+    ll = "-Mlocal::lib=#{resource[:local_lib]}" if resource[:local_lib]
+    current = `perl #{ll} -M#{resource[:name]} -e 'print $#{resource[:name]}::VERSION' 2>/dev/null;`
+    return false if current == ''
+    cpan_str = `perl #{ll} -e 'use CPAN; my $mod=CPAN::Shell->expand("Module","#{resource[:name]}"); printf("%s", $mod->cpan_version eq "undef" || !defined($mod->cpan_version) ? "-" : $mod->cpan_version);'`
+    latest = cpan_str.match(/^[a-zA-Z]?([0-9]+.?[0-9]*\.?[0-9]*)$/)[1]
+    if Puppet::Util::Package.versioncmp(latest.chomp, current.chomp) > 0
+      return true
     end
-    current_version=`perl #{ll} -M#{resource[:name]} -e 'print $#{resource[:name]}::VERSION'`
-    cpan_str=`perl #{ll} -e 'use CPAN; my $mod=CPAN::Shell->expand("Module","#{resource[:name]}"); printf("%s", $mod->cpan_version eq "undef" || !defined($mod->cpan_version) ? "-" : $mod->cpan_version);'`
-    latest_version=cpan_str.match(/^[0-9]+.?[0-9]*$/)[0]
-    current_version.chomp
-    latest_version.chomp
-    if current_version < latest_version
-    return false else return true end
+    false
   end
 
   def create
     Puppet.info("Installing cpan module #{resource[:name]}")
-    if resource[:local_lib]
-      ll = "-Mlocal::lib=#{resource[:local_lib]}"
-    end
-
+    ll = "-Mlocal::lib=#{resource[:local_lib]}" if resource[:local_lib]
     umask = "umask #{resource[:umask]};" if resource[:umask]
-
     Puppet.debug("cpan #{resource[:name]}")
     if resource.force?
       Puppet.info("Forcing install for #{resource[:name]}")
@@ -41,24 +34,20 @@ Puppet::Type.type(:cpan).provide( :default ) do
       system("#{umask} yes | perl #{ll} -MCPAN -e 'CPAN::install #{resource[:name]}'")
     end
 
-    #cpan doesn't always provide the right exit code, so we double check
+    # cpan doesn't always provide the right exit code, so we double check
     system("perl #{ll} -M#{resource[:name]} -e1 > /dev/null 2>&1")
-    estatus = $?.exitstatus
+    estatus = $CHILD_STATUS.exitstatus
 
-    if estatus != 0
-      raise Puppet::Error, "cpan #{resource[:name]} failed with error code #{estatus}"
-    end
+    raise Puppet::Error, "cpan #{resource[:name]} failed with error code #{estatus}" if estatus != 0
   end
 
-  def destroy
-  end
-  
+  def destroy; end
+
   def update
     Puppet.info("Upgrading cpan module #{resource[:name]}")
     Puppet.debug("cpan #{resource[:name]}")
-    if resource[:local_lib]
-      ll = "-Mlocal::lib=#{resource[:local_lib]}"
-    end
+
+    ll = "-Mlocal::lib=#{resource[:local_lib]}" if resource[:local_lib]
     umask = "umask #{resource[:umask]};" if resource[:umask]
 
     if resource.force?
@@ -67,20 +56,17 @@ Puppet::Type.type(:cpan).provide( :default ) do
     else
       system("#{umask} yes | perl #{ll} -MCPAN -e 'CPAN::install #{resource[:name]}'")
     end
-    estatus = $?.exitstatus
-    
-    if estatus != 0
-      raise Puppet::Error, "CPAN::install #{resource[:name]} failed with error code #{estatus}"
-    end
+    estatus = $CHILD_STATUS.exitstatus
+
+    raise Puppet::Error, "CPAN::install #{resource[:name]} failed with error code #{estatus}" if estatus != 0
   end
 
   def exists?
-    if resource[:local_lib]
-      ll = "-Mlocal::lib=#{resource[:local_lib]}"
-    end
+    ll = "-Mlocal::lib=#{resource[:local_lib]}" if resource[:local_lib]
+
     Puppet.debug("perl #{ll} -M#{resource[:name]} -e1 > /dev/null 2>&1")
     output = `perl #{ll} -M#{resource[:name]} -e1 > /dev/null 2>&1`
-    estatus = $?.exitstatus
+    estatus = $CHILD_STATUS.exitstatus
 
     case estatus
     when 0
@@ -92,5 +78,4 @@ Puppet::Type.type(:cpan).provide( :default ) do
       raise Puppet::Error, "perl #{ll} -M#{resource[:name]} -e1 failed with error code #{estatus}: #{output}"
     end
   end
-
 end
